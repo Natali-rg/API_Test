3c8f0e8f572d1e793ee2ac80d42aa1027e60ccae
amenson1983 <amenson1983@gmail.com>
2022-09-14 18:11:19 +0300

    API testing+HomeWork_Corrected lesson_24 HomeWork_Request

--- src/main/java/TestDB.java
+++ src/main/java/TestDB.java
@@ -1,28 +1,31 @@
-import data.User;
-import driverConfig.BROWSER;
-import driverConfig.DriverFactory;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import util.sql.SQLInstans;
 import util.sql.StatemenT;
 
 public class TestDB {
     static StatemenT dbDriver;
+
+
     static WebDriver driver;
     @BeforeClass
     public static void start(){
         dbDriver= SQLInstans.getInstance();
-        driver= DriverFactory.getDriver(BROWSER.CHROME);
+
+//        driver= DriverFactory.getDriver(BROWSER.CHROME);
     }
     @Test
     public void test(){
-        driver.get("https://accounts.google.com/AccountChooser/identifier?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=AccountChooser");
+//        driver.get("https://accounts.google.com/AccountChooser/identifier?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=AccountChooser");
 //        dbDriver.selectUsersByName("Mike");
-        User user=dbDriver.getUserByid("118");
+//        User user=dbDriver.getUserByid("118");
 //        System.out.println(user.getEmail());
-        driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys(user.getEmail());
+//        driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys(user.getEmail());
+//        dbDriver.insert("Natik","natik@gmail.com", "f4567g","g45dd54r456d");
+//        dbDriver.delete("g45dd54r456d");
+//        dbDriver.updatePassword("Qwerty","123");
+        dbDriver.selectUsersByEmail("natik@gmail.com");
 
     }
 }
--- src/main/java/util/sql/StatemenT.java
+++ src/main/java/util/sql/StatemenT.java
@@ -6,6 +6,9 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class StatemenT {
     private Statement statement;
@@ -23,29 +26,19 @@ public class StatemenT {
         return statement;
     }
 
-    public void selectByUserNameEmail(String username, String email) {
-        String s = "SELECT * FROM sys.user where username=\"" + username + "\" and email=\"" + email + "\"";
-        try {
-            ResultSet resultSet=statement.executeQuery(s);//запрос с ответом где resultSet это ответ
-            resultSet.next();
-            System.out.println(resultSet.getString("username"));
-            System.out.println(resultSet.getString("password"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public void selectUsersByName(String UserName){
-        String s="SELECT * FROM sys.user where username=\""+UserName+"\"";
+    public void selectUsersByEmail(String email){//HomeWork список users с одинаковым email
+        String s="SELECT * FROM sys.user where email=\""+email+"\";";
         try {
             ResultSet resultSet=statement.executeQuery(s);
             while (resultSet.next()){
-                System.out.println(resultSet.getString("password"));
+                System.out.println(resultSet.getString("username"));
+                System.out.println(resultSet.getString("id"));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    public User getUserByid(String id){
+    public User getUserByid(String id){//HomeWork список users по id
         String s="SELECT * FROM sys.user where id=\""+id+"\"";
         User user=null;
         try {
@@ -60,4 +53,90 @@ public class StatemenT {
         }
         return user;
     }
+
+    public void updatePassword(String pass, String id){//HomeWork обновление пароля
+        String s="UPDATE `sys`.`user` SET `password` = \""+pass+"\" WHERE (`id` = \""+id+"\");";
+        try {
+            statement.execute(s);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+   /* public void selectByUserNameEmail(String username, String email) {
+        String s = "SELECT * FROM sys.user where username=\"" + username + "\" and email=\"" + email + "\"";
+        try {
+            ResultSet resultSet=statement.executeQuery(s);//запрос с ответом где resultSet это ответ
+            resultSet.next();
+            System.out.println(resultSet.getString("username"));
+            System.out.println(resultSet.getString("password"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void selectUsersByName(String UserName){
+        String s="SELECT * FROM sys.user where username=\""+UserName+"\"";
+        try {
+            ResultSet resultSet=statement.executeQuery(s);
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("password"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void insert (String username,String email,String password){
+        UUID uuid=UUID.randomUUID();
+        String s="INSERT INTO `sys`.`user` (`username`, `email`, `password`, `id`) " +
+                "VALUES (\""+username+"\", \""+email+"\", \""+password+"\", \""+uuid.toString()+"\");";
+        try {
+            statement.execute(s);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insert (String username,String email, String password, String id){
+        String s="INSERT INTO `sys`.`user` (`username`, `email`, `password`, `id`) " +
+                "VALUES (\""+username+"\", \""+email+"\", \""+password+"\", \""+id+"\");";
+        try {
+            statement.execute(s);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(String id){
+        String s="DELETE FROM `sys`.`user` WHERE (`id` = '"+id+"');";
+        try {
+            statement.execute(s);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public List<User> getListAllUsers(){
+        String s="SELECT * FROM sys.user;";
+        List<User> users=new ArrayList<>();
+        try {
+            ResultSet resultSet=statement.executeQuery(s);
+            while (resultSet.next()){
+                users.add(new User(resultSet.getString("username"),
+                        resultSet.getString("email"),
+                        resultSet.getString("password"),
+                        resultSet.getString("id")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
+    }*/
+
 }
